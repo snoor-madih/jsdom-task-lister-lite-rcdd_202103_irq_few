0ce24ee11c78291a7b2c712c9a8c3af237a30fde
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:28:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById("new-task-description");
+  const submit = document.getElementByTagName("input");
+  
+  const ul=document.getElementById('tasks');
+
+submit.addEventListener("click",function(event)){
+  
+event.preventDefault
+document.body.append("ul");
+for (var i=0; i<100; i++){
+
+    var li=document.createElement('li');
+
+    ul.appendChild(li);
+    li.innerHTML=li.innerHTML + input.innerHTML;
+}
+}
 });
